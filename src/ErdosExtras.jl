f994c52d587fcdef5a05feeb95bc3544595c585c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 21:02:03 +0000

    Snapshot of branch dev taken on 2019-04-07T17:02:01.882-04:00 from "https://github.com/UnofficialJuliaMirror/ErdosExtras.jl-36e9a02b-89ba-5c81-9f23-352891f35b31"

---
+++ src/ErdosExtras.jl
@@ -0,0 +1,25 @@
+__precompile__()
+module ErdosExtras
+using Erdos
+using JuMP
+using GLPKMathProgInterface
+
+# export set_lp_solver, set_mip_solver, get_lp_solver, get_mip_solver
+
+export minimum_weight_perfect_bmatching
+
+export solve_tsp
+
+LP_SOLVER = GLPKSolverLP()   #GurobiSolver(OutputFlag=0)
+MIP_SOLVER = GLPKSolverMIP()
+
+# set_lp_solver(solv) = (LP_SOLVER=solv; solv)
+# set_mip_solver(solv) = (MIP_SOLVER=solv; solv)
+#
+# get_lp_solver() = LP_SOLVER
+# get_mip_solver() = MIP_SOLVER
+
+include("matching/Matching.jl")
+include("tsp/TSP.jl")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,29 @@
+using Erdos
+using ErdosExtras
+using Base.Test
+
+testdir = dirname(@__FILE__)
+
+tests = [
+    "tsp/TSP",
+    "matching/bmatching"
+    ]
+
+GLIST =    [
+            (Graph{Int64}, DiGraph{Int64}),
+            (Graph{UInt32}, DiGraph{UInt32}),
+            (Network, DiNetwork)
+            ]
+
+for GDG in GLIST
+    global G = GDG[1]
+    global DG = GDG[2]
+    global E = edgetype(G)
+    global V = vertextype(G)
+    # @testset "$G" begin
+    println("@@@@ Testing $G")
+    for t in tests
+        include(joinpath(testdir,"$(t).jl"))
+    end
+    # end
+end
